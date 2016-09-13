6750e812d68757572fbff6d8fdcb9596d535d520
Vijay <saradhiv@quindell.com>
2016-09-13 13:18:11 +0100

    EQUITY-2986: initial changes committed.

---
+++ src/main/java/com/hubio/integration/services/vehicle/VehicleDetailsLookupService.java
@@ -0,0 +1,18 @@
+package com.hubio.integration.services.vehicle;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * Created by Saradhi on 09/09/2016.
+ */
+public interface VehicleDetailsLookupService {
+
+    @POST
+    @Path("/retrieveVehicleDetails")
+    @Consumes({"text/xml"})
+    @Produces({"text/xml"})
+    VehicleLookupResponse  retrieveVehicleDetails(String vrm);
+}
