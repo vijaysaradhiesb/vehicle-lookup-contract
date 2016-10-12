ab5d1362ac5faa1c7daa4a0b784389490407f7da
Vijay <saradhiv@quindell.com>
2016-10-12 09:32:26 +0100

    ICE-2650: updated the contract model to work with XML and JSON request/response.

--- src/main/java/com/hubio/integration/services/vehicle/VehicleDetailsLookupService.java
+++ src/main/java/com/hubio/integration/services/vehicle/VehicleDetailsLookupService.java
@@ -4,6 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by Saradhi on 09/09/2016.
@@ -12,7 +13,7 @@ public interface VehicleDetailsLookupService {
 
     @POST
     @Path("/retrieveVehicleDetails")
-    @Consumes({"text/xml"})
-    @Produces({"text/xml"})
+    @Consumes({MediaType.APPLICATION_JSON,MediaType.TEXT_XML})
+    @Produces({MediaType.APPLICATION_JSON,MediaType.TEXT_XML})
     VehicleLookupResponse  retrieveVehicleDetails(VehicleLookupRequest vehicleLookupRequest);
 }
