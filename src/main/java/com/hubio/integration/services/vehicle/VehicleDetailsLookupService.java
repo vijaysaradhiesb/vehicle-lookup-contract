80112428514ac70acfb872211eba4eb4fac52549
Piotr Klimczak <KlimczakP@quindell.com>
2016-12-06 10:27:32 +0000

    ICE-2912 Removing JSON as we do not support JSON.

--- src/main/java/com/hubio/integration/services/vehicle/VehicleDetailsLookupService.java
+++ src/main/java/com/hubio/integration/services/vehicle/VehicleDetailsLookupService.java
@@ -15,7 +15,7 @@ public interface VehicleDetailsLookupService {
 
     @POST
     @Path("/retrieveVehicleDetails")
-    @Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
-    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+    @Consumes({MediaType.TEXT_XML})
+    @Produces({MediaType.TEXT_XML})
     ContractResponse retrieveVehicleDetails(VehicleLookupRequest vehicleLookupRequest);
 }
